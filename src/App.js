3bb2fa11a75d7746625864db6f7a1f5c9bf22348
octocatblain <blainmasaku2@gmail.com>
2023-12-09 22:37:01 +0300

    add useeffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 //27d0821
 
-const API_URL = "https://WWW.omdbapi.com?apikey=27d0821";
+const API_URL = "https://www.omdbapi.com?apikey=27d0821";
 
 const App = () => {
+  const fetchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchMovies("batman");
+  }, []);
   return (
     <div>
       <h1>Hello World!</h1>
