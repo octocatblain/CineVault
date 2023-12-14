a131d380ef2ef9ea446524927c5d39b3cee057bd
octocatblain <blainmasaku2@gmail.com>
2023-12-14 13:55:42 +0300

    feat: create moviecard component

--- src/App.js
+++ src/App.js
@@ -1,59 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
-//27d0821
+//API Key = 27d0821
 
-const API_URL = "https://www.omdbapi.com?apikey=27d0821";
+const API_URL = "https://www.omdbapi.com/?apikey=27d0821&";
+// const IMG_URL = "http://img.omdbapi.com/?apikey=27d0821&";
+const movie1 = {
+  Title: "Batman Begins",
+  Year: "2005",
+  imdbID: "tt0372784",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+};
 
 const App = () => {
   const fetchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data);
+    console.log(data.search);
   };
 
   useEffect(() => {
     fetchMovies("batman");
   }, []);
+
   return (
     <div className="app">
       <h1>CineVault</h1>
 
       <div className="search">
         <input
-          type="text"
-          className="search-bar"
           placeholder="Search for a movie..."
           value="batman"
-          onChange={(e) => fetchMovies(e.target.value)}
+          onChange={() => {}}
         />
-        <img src={SearchIcon} alt="search icon" onClick={() => fetchMovies()} />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
       </div>
 
       <div className="container">
-        <div className="movie">
-          <img
-            src="https://images-na.ssl-images-amazon.com/images/I/71niXI3lxlL._AC_SY879_.jpg"
-            alt="movie poster"
-          />
-          <div className="movie-info">
-            <h3>Movie Title</h3>
-            <span className="green">PG-13</span>
-          </div>
-          <div className="overview">
-            <h3>Overview</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              voluptates, voluptatem, atque, quas quod quidem doloremque
-              voluptatum quia dolorum nemo quibusdam repellat. Quisquam
-              voluptates, voluptatem, atque, quas quod quidem doloremque
-              voluptatum quia dolorum nemo quibusdam repellat.
-            </p>
-            <button className="btn">View Details</button>
-          </div>
-        </div>
+        <MovieCard />
       </div>
     </div>
   );
