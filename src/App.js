b6f2b7e15bba708bfa2c586d0ec5e46a15ec3981
octocatblain <blainmasaku2@gmail.com>
2023-12-14 14:49:42 +0300

    feat: add search function

--- src/App.js
+++ src/App.js
@@ -7,21 +7,16 @@ import MovieCard from "./Components/MovieCard";
 
 const API_URL = "https://www.omdbapi.com/?apikey=27d0821&";
 // const IMG_URL = "http://img.omdbapi.com/?apikey=27d0821&";
-const movie1 = {
-  Title: "Batman Begins",
-  Year: "2005",
-  imdbID: "tt0372784",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-};
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
   const fetchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -35,15 +30,27 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for a movie..."
-          value="batman"
-          onChange={() => {}}
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
         />
-        <img src={SearchIcon} alt="search" onClick={() => {}} />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => fetchMovies(searchValue)}
+        />
       </div>
 
-      <div className="container">
-        <MovieCard movie={movie1} />
-      </div>
+      {movies.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.imdbID} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h1>Movie Database is Null!</h1>
+        </div>
+      )}
     </div>
   );
 };
