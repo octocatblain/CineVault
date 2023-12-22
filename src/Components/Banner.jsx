9e043f31edbfa5a1fcd7b6c82b9aca8f573b6c2c
octocatblain <blainmasaku2@gmail.com>
2023-12-22 16:42:28 +0300

    feat: add banner component

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h4>WEBSITE IN DEVELOPMENT</h4>
+    </div>
+  );
+};
+
+export default Banner;
